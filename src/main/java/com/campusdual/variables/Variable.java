6a45345347d2d94fdc713cfeb0698ef27b41d7d1
Gael Santome Lopez <you@example.comgaelsantome@gmail.com git config --global user.name Gasel>
2024-10-09 16:08:53 +0200

    Ejercicio 3

--- src/main/java/com/campusdual/variables/Variable.java
+++ src/main/java/com/campusdual/variables/Variable.java
@@ -1,4 +1,12 @@
 package com.campusdual.variables;
-
+import java.sql.SQLOutput;
 public class Variable {
+    public static void main(String[] args) {
+        double hours=2.5;
+        int month=12;
+        String name ="Ramiro";
+        final int DAY_HOURS = 24;
+        name = "Gael";
+        System.out.println(DAY_HOURS);
+    }
 }
