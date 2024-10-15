e9d518fc0227e4792312f964a0788b354238a0db
Gael Santome Lopez <you@example.comgaelsantome@gmail.com git config --global user.name Gasel>
2024-10-15 10:12:26 +0200

    Clase 15/10

---
+++ src/main/java/com/campusdual/classes/anotherPackage/AnotherClass.java
@@ -0,0 +1,13 @@
+package com.campusdual.classes.anotherPackage;
+
+import com.campusdual.classes.Book;
+
+public class AnotherClass {
+    public static void main(String[] args) {
+        Book b1 = new Book("Rythm of War", "Brandon Sanderson", 1902);
+        Book b2 = new Book("The Magician's Apprentice", "Trudi Canavan", 781);
+      //  b1.showBookDetails();
+      //  b2.showBookDetails();
+      //  System.out.println(b1.title);
+    }
+}
