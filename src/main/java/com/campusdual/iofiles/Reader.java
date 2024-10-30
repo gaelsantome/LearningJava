d9484ad62930f9e67ff2cc2dc26577e439d3ec7a
Gael Santome Lopez <you@example.comgaelsantome@gmail.com git config --global user.name Gasel>
2024-10-30 09:31:49 +0100

    Writer, reader y delete

---
+++ src/main/java/com/campusdual/iofiles/Reader.java
@@ -0,0 +1,25 @@
+package com.campusdual.iofiles;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Reader {
+    public static void main(String[] args) {
+        Path filePath = Paths.get(Route.PATH_NAME);
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath.toFile()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            //throw new RuntimeException(e);
+        }
+
+    }
+}
+
+
